89e3889ee2d0dbaac065246a5d196d7687e50a69
Zhmayev Yaroslav <salaros@yandex.ru>
2015-12-29 18:31:49 +0300

    adding NuGet packaging and bumped version to 0.2

--- packaging/Properties/AssemblyInfo.cs
+++ packaging/Properties/AssemblyInfo.cs
@@ -1,15 +1,19 @@
-using System.Resources;
+﻿using System.Resources;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetworkAgilityPack")]
-[assembly: AssemblyDescription("A set of tools that help developers to interact with Web and other Internet resources")]
-[assembly: AssemblyCompany("CodeCave")]
-[assembly: AssemblyProduct("NetworkAgilityPack")]
+[assembly: AssemblyTitle("CodeCave.NetworkAgilityPack.NuGet")]
+[assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeCave.NetworkAgilityPack.NuGet")]
 [assembly: AssemblyCopyright("Copyright © CodeCave 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -17,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("26023ab0-6421-4764-9d93-534cc021fe04")]
+[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,6 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
