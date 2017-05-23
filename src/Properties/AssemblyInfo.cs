83921a2a9c0f738aed6ff3326e93061a190ce141
salaros <salaros@yandex.ru>
2017-05-23 10:51:19 +0300

    bumping NAP version to v0.3.6

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-using System.Resources;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.5")]
+[assembly: AssemblyVersion("0.3.6")]
 [assembly: NeutralResourcesLanguage("en")]
 
